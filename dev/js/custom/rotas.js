b6a89bcf309a1a7b7e57a021258239823e16f9f5
Relison Andrade Galvão <b122692866@erj5153.ipea.gov.br>
2019-08-19 12:34:25 -0300

    rotas.js homologa

--- dev/js/custom/rotas.js
+++ dev/js/custom/rotas.js
@@ -6,7 +6,8 @@ class Rotas{
   getBaseUrl(){
     var loc = window.location;
   //  return loc.protocol + '//' + loc.host + '/api/';//:' + loc.port + '/api/';
-    return  "https://mapaosc-desenv.ipea.gov.br/api/";
+    return  "https://mapaosc-homologa.ipea.gov.br/api/";
+    //return  "https://mapaosc-desenv.ipea.gov.br/api/";
     //return  "http://localhost:9090/api/";
     //return  "http://localhost:8009/api/";
   }
